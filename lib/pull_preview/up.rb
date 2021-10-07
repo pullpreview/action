9799cae33b9c04598f45b9ffb4b92bfdf7fc6639
mfittko <manuel.fittko@sofatutor.com>
2021-10-07 12:19:06 +0200

    tar exclude files as specified in dockerignore

--- lib/pull_preview/up.rb
+++ lib/pull_preview/up.rb
@@ -21,7 +21,7 @@ module PullPreview
       instance_name = opts[:name]
 
       PullPreview.logger.info "Taring up repository at #{app_path.inspect}..."
-      unless system("tar czf /tmp/app.tar.gz --exclude .git -C '#{app_path}' .")
+      unless system("tar czf /tmp/app.tar.gz --exclude-from=.dockerignore -C '#{app_path}' .")
         exit 1
       end
 
