2cda4f948405c437111265a652fb6b0fe58482bf
Cyril Rohr <cyril.rohr@gmail.com>
2021-12-01 20:43:49 +0000

    fix

--- lib/pull_preview/instance.rb
+++ lib/pull_preview/instance.rb
@@ -267,10 +267,8 @@ module PullPreview
         f.puts access_details.cert_key
       end
       [key_file_path].each{|file| FileUtils.chmod 0600, file}
-      logger.debug "key_file_content=#{File.read(key_file_path).inspect}"
-      logger.debug "cert_key_content=#{File.read(cert_key_path).inspect}"
 
-      cmd = "ssh #{"-vvv " if logger.level == Logger::DEBUG}-o ServerAliveInterval=15 -o IdentitiesOnly=yes -i #{key_file_path} #{ssh_address} #{ssh_options.join(" ")} '#{command}'"
+      cmd = "ssh #{"-v " if logger.level == Logger::DEBUG}-o ServerAliveInterval=15 -o IdentitiesOnly=yes -i #{key_file_path} #{ssh_address} #{ssh_options.join(" ")} '#{command}'"
       if input && input.respond_to?(:path)
         cmd = "cat #{input.path} | #{cmd}"
       end
