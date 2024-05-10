eabb077369892628bf284a9f86bf67abbd312e5d
Bryan Bojorque <bojorquebryan@gmail.com>
2024-05-10 09:51:15 +0800

    terminate instance requires a few more opts

--- lib/pull_preview/down.rb
+++ lib/pull_preview/down.rb
@@ -1,7 +1,7 @@
 module PullPreview
   class Down
     def self.run(opts)
-      instance = Instance.new(opts[:name])
+      instance = Instance.new(opts[:name], opts)
       PullPreview.logger.info "Deleting the subdomain to DNS Zone"
       instance.delete_domain_entry
 
